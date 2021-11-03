6f86a11dbcb27f4e0672f3ede7239a604afe03bb
Thomas Patrick Levy <thomasplevy@gmail.com>
2021-11-03 10:46:22 -0700

    add a beta message / warning to the command's root help doc

--- src/Commands/Root.php
+++ src/Commands/Root.php
@@ -5,7 +5,7 @@
  * @package LifterLMS/CLI
  *
  * @since 0.0.1
- * @version 0.0.2
+ * @version [version]
  */
 
 namespace LifterLMS\CLI\Commands;
@@ -13,7 +13,16 @@ namespace LifterLMS\CLI\Commands;
 /**
  * Manage LifterLMS.
  *
- * @since [version]
+ * ## BETA WARNING
+ *
+ * The LLMS-CLI is currently in early release as an open public beta. Commands
+ * are subject to change without warning. Please pay close attention to the
+ * changelog as we continue to develop and improve the CLI.
+ *
+ * If you encounter any issues or wish to provide feedback on the LLMS-CLI
+ * please get in touch at https://github.com/gocodebox/lifterlms-cli.
+ *
+ * @since 0.0.1
  */
 class Root extends AbstractCommand {
 
