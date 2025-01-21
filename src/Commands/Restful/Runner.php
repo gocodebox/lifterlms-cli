9a296d7d0d8ec77d94471b44a8ec079d56ca6060
Brian Hogg <brian@lifterlms.com>
2025-01-21 15:50:56 -0500

    Fix strlen null error in PHP 8.3 (#14)
    * Check for null in resource ID before trying to get the string length.
    * Removing workflow (no phpunit tests).

--- src/Commands/Restful/Runner.php
+++ src/Commands/Restful/Runner.php
@@ -145,7 +145,7 @@ class Runner {
 			$parsed_args   = preg_match_all( '#\([^\)]+\)#', $route, $matches );
 			$resource_id   = ! empty( $matches[0] ) ? array_pop( $matches[0] ) : null;
 			$trimmed_route = rtrim( $route );
-			$is_singular   = $resource_id === substr( $trimmed_route, - strlen( $resource_id ) );
+			$is_singular   = $resource_id === substr( $trimmed_route, - strlen( $resource_id ?? '' ) );
 
 			// List a collection
 			if ( array( 'GET' ) == $endpoint['methods']
